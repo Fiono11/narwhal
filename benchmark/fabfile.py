672a070391287775c63e8a022c418dc732116834
Fiono11 <ruipedromorais11@gmail.com>
2023-06-04 18:10:03 +0100

    wip

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -103,7 +103,7 @@ def remote(ctx, debug=False):
     ''' Run benchmarks on AWS '''
     bench_params = {
         'faults': 0,
-        'nodes': [4],
+        'nodes': [10],
         'workers': 1,
         'collocate': True,
         'rate': [500],
