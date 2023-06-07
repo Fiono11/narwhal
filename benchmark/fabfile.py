adc7790e1b9107264b9bbdabbe619cf19f768752
Fiono11 <ruipedromorais11@gmail.com>
2023-06-07 15:33:39 +0100

    wip

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -107,7 +107,7 @@ def remote(ctx, debug=False):
         'workers': 1,
         'collocate': True,
         'rate': [1000],
-        'tx_size': 4169,
+        'tx_size': 6321,
         'duration': 330,
         'runs': 1,
     }
