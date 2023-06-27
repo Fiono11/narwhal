95e75c36b38f428dade1f9e53e2e1711f03d5dfb
Fiono11 <ruipedromorais11@gmail.com>
2023-06-27 09:54:39 +0100

    wip

--- primary/src/proposer.rs
+++ primary/src/proposer.rs
@@ -162,17 +162,17 @@ impl Proposer {
                     //let mut elections_ids = BTreeSet::new();
 
                     //#[cfg(feature = "benchmark")]
-                    //for (_tx_hash, election_id) in &header.votes {
+                    for (_tx_hash, election_id) in &header.votes {
                         //info!("Created1 {} -> {:?}", tx_hash, election_id);
                         //elections_ids.insert(election_id.clone());
                         //if !self.active_elections.contains(&election_id)
                             //&& !self.decided.contains(&election_id)
                         //{
                             // NOTE: This log entry is used to compute performance.
-                            //self.active_elections.insert(election_id.clone());
-                            //info!("Added {} to active elections", election_id.clone());
+                            self.active_elections.insert(election_id.clone());
+                            info!("Added {} to active elections2", election_id.clone());
                         //}
-                    //}
+                    }
                 }
             }
 
