4c9d02f49aa9a073187d192667f911bc463ed01c
Fiono11 <ruipedromorais11@gmail.com>
2023-06-27 21:54:55 +0100

    wip

--- primary/src/proposer.rs
+++ primary/src/proposer.rs
@@ -5,7 +5,7 @@ use config::{Committee, WorkerId};
 use crypto::Hash as _;
 use crypto::{Digest, PublicKey, SignatureService};
 use log::{debug, info};
-#[cfg(feature = "benchmark")]
+//#[cfg(feature = "benchmark")]
 use log::info;
 use tokio::sync::mpsc::{Receiver, Sender};
 use tokio::time::{sleep, Duration, Instant};
